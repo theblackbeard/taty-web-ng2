36e4edc833aa70f91d15ac69406128bac85973cc
BlackBeard <ccarvalhos@outlook.com>
2017-05-07 17:10:27 -0300

    Front Added

--- src/assets/js/scripts.js
+++ src/assets/js/scripts.js
@@ -1,10 +1,44 @@
 ﻿$(document).ready(function(){
+    let form = $("#submitEmail");
+    $(form).submit(function(e){
+        e.preventDefault();
+        let name = $("#name").val();
+        let email = $("#email").val();
+        let body = $("#body").val();
+        let query = {};
 
-   $("li").hover(function(){
-       $(this).addClass("wBorder");
-   }, function(){
-       $(this).removeClass("wBorder");
-   })     
+        if(name == '' | email == '' | body == ''){
+            $("#mt-errors").html("<p>Por favor, preencha todos campos obrigatórios</p>");
+        }else{
+           query = {
+               "name" : name,
+               "email": email,
+               "body": body
+           }
+          
+           $.ajax({
+               url: "https://formspree.io/tatianasouza01@gmail.com",
+               method: "POST",
+               beforeSend: function(){
+                   $("#mt-wait").show();
+                   $('html, body').animate({scrollTop: ( $("#mt-top").offset().top )}, 500);
+                   $('.modal').css({'display': 'block'});
+                 },
+               complete: function(){$("#mt-wait").fadeOut('slow');  $('.modal').css({'display': 'none'});},
+               data: query,
+               dataType: "json",
+           }).
+            done(function(data){
+               $(form)[0].reset();
+               console.log(data);
+             
+            })
+
+        }
+      
+
+    })
+
 
    // init Masonry
    var $grid = $('.grid').masonry({
